cc2848e290340fef421d38b7835dd8318610de1a
Dienel Leandro Leon Vizarres <142622837+DienelLeon19@users.noreply.github.com>
2023-10-24 23:41:13 -0500

    Laboratorio10

--- Laboratorio10/src/laboratorio10/Camara.java
+++
@@ -1,9 +0,0 @@
-    package laboratorio10;
-
-public interface Camara
-{
-    public void tomarfoto();
-    public void grabarvideo();
-    public void zoom();
-    public void efecto();
-}
--- Laboratorio10/src/laboratorio10/NavegadorDeInternet.java
+++
@@ -1,9 +0,0 @@
-    package laboratorio10;
-
-public interface NavegadorDeInternet
-{
-    public void buscar();
-    public void favoritos();
-    public void historial();
-    public void recargarpagina();
-}
